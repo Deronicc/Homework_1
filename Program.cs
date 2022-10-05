794048d93c826e63662b8db73398b9847b743a72
Kidyaev_I <kideav94@gmail.com>
2022-10-05 21:56:39 +0300

    добавил комментарии для себя

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3 */
 
-/* Console.WriteLine("____________________");
+Console.WriteLine("____________________");
 Console.WriteLine("Задача 1");
 Console.WriteLine();
 Console.WriteLine("Введите первое число");
@@ -17,21 +17,21 @@ if (Num1 < Num2)
 {
     Console.WriteLine($"NUM0001 = {Num1}; b = {Num2} -> max = {Num2}");
 }
-else if (Num1 > Num2)
+else if (Num1 > Num2) // вложенный if 
 {
     Console.WriteLine($"NUM0001 = {Num1}; b = {Num2} -> max = {Num1}");
 }
 else
 {
     Console.WriteLine("Числа равны. Нет максимального числа");
-} */
+}
 
 /* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 /* 2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22 */
 
-/* Console.WriteLine("____________________");
+Console.WriteLine("____________________");
 Console.WriteLine("Задача 2");
 Console.WriteLine();
 Console.WriteLine("Введите первое число");
@@ -46,21 +46,21 @@ Console.WriteLine();
 Console.Write("Ответ: ");
 if (max < Num02)
 {
-    max = Num02;
+    max = Num02; // присвоение "max" новому числу
 }
 if (max < Num03)
 {
     max = Num03;
 }
 Console.WriteLine($"{Num01}, {Num02}, {Num03} -> {max}");
- */
+
 /* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
 является ли число чётным (делится ли оно на два без остатка).
 4 -> да
 -3 -> нет
 7 -> нет */
 
-/* Console.WriteLine("____________________");
+Console.WriteLine("____________________");
 Console.WriteLine("Задача 3");
 Console.WriteLine();
 Console.WriteLine("Введите число: ");
@@ -68,7 +68,7 @@ int Num001 = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine();
 Console.Write("Ответ: ");
-if (Num001%2==0)
+if (Num001%2==0) // % деление без остатка
 {
     Console.WriteLine($"{Num001} -> да");
 }
@@ -76,7 +76,7 @@ else
 {
     Console.WriteLine($"{Num001} -> нет");
 }
- */
+
 /* Задача 8: Напишите программу, которая на вход принимает число (N),
 а на выходе показывает все чётные числа от 1 до N.
 
@@ -92,14 +92,14 @@ int a = 1;
 
 Console.WriteLine();
 Console.Write("Ответ: ");
-while (a <= NUM0001)
+while (a <= NUM0001) // Оператор while позволяет повторять выполнение оператора до тех пор, пока указанное выражение не станет ложным.
 {
-    if (a%2==0)
+    if (a%2==0) 
     {
         Console.Write($"{a}, ");
 
     }
     a++;
 }
-Console.SetCursorPosition(Console.CursorLeft - 2, Console.CursorTop);
-Console.WriteLine("  ");
+Console.SetCursorPosition(Console.CursorLeft - 2, Console.CursorTop); // удаление лишней запятой в конце (удаление 2 символов в конце)
+Console.WriteLine("  "); // удаление лишней запятой в конце (вставить пустую строчку с пробелами. без этого прошлая операция не работает)
