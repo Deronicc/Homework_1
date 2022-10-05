75d25ab79d1700a5f13e21cd93ba5c10772ecff7
Kidyaev_I <kideav94@gmail.com>
2022-10-05 21:46:35 +0300

    решение 4 задачи (задача 8) и поправил 2 задачу

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3 */
 
-Console.WriteLine("____________________");
+/* Console.WriteLine("____________________");
 Console.WriteLine("Задача 1");
 Console.WriteLine();
 Console.WriteLine("Введите первое число");
@@ -15,24 +15,23 @@ Console.WriteLine();
 Console.Write("Ответ: ");
 if (Num1 < Num2)
 {
-
-    Console.WriteLine($"a = {Num1}; b = {Num2} -> max = {Num2}");
+    Console.WriteLine($"NUM0001 = {Num1}; b = {Num2} -> max = {Num2}");
 }
 else if (Num1 > Num2)
 {
-    Console.WriteLine($"a = {Num1}; b = {Num2} -> max = {Num1}");
+    Console.WriteLine($"NUM0001 = {Num1}; b = {Num2} -> max = {Num1}");
 }
 else
 {
     Console.WriteLine("Числа равны. Нет максимального числа");
-}
+} */
 
 /* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 /* 2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22 */
 
-Console.WriteLine("____________________");
+/* Console.WriteLine("____________________");
 Console.WriteLine("Задача 2");
 Console.WriteLine();
 Console.WriteLine("Введите первое число");
@@ -47,41 +46,60 @@ Console.WriteLine();
 Console.Write("Ответ: ");
 if (max < Num02)
 {
-    Console.WriteLine($"{Num01}, {Num02}, {Num03} -> {Num02}");
+    max = Num02;
 }
-else if (Num02 < Num03)
+if (max < Num03)
 {
-    Console.WriteLine($"{Num01}, {Num02}, {Num03} -> {Num03}");
+    max = Num03;
 }
-else
-{
-    Console.WriteLine($"{Num01}, {Num02}, {Num03} -> {max}");
-}
-
+Console.WriteLine($"{Num01}, {Num02}, {Num03} -> {max}");
+ */
 /* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
 является ли число чётным (делится ли оно на два без остатка).
 4 -> да
 -3 -> нет
 7 -> нет */
 
-Console.WriteLine("____________________");
+/* Console.WriteLine("____________________");
 Console.WriteLine("Задача 3");
 Console.WriteLine();
 Console.WriteLine("Введите число: ");
-int Num = int.Parse(Console.ReadLine()!);
+int Num001 = int.Parse(Console.ReadLine()!);
 
-if (Num%2==0)
+Console.WriteLine();
+Console.Write("Ответ: ");
+if (Num001%2==0)
 {
-    Console.WriteLine();
-    Console.WriteLine($"{Num} -> да");
+    Console.WriteLine($"{Num001} -> да");
 }
 else
 {
-    Console.WriteLine();
-    Console.WriteLine($"{Num} -> нет");
+    Console.WriteLine($"{Num001} -> нет");
 }
-
-/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+ */
+/* Задача 8: Напишите программу, которая на вход принимает число (N),
+а на выходе показывает все чётные числа от 1 до N.
 
 5 -> 2, 4
 8 -> 2, 4, 6, 8 */
+
+Console.WriteLine("____________________");
+Console.WriteLine("Задача 4");
+Console.WriteLine();
+Console.WriteLine("Введите число: ");
+int NUM0001 = int.Parse(Console.ReadLine()!);
+int a = 1;
+
+Console.WriteLine();
+Console.Write("Ответ: ");
+while (a <= NUM0001)
+{
+    if (a%2==0)
+    {
+        Console.Write($"{a}, ");
+
+    }
+    a++;
+}
+Console.SetCursorPosition(Console.CursorLeft - 2, Console.CursorTop);
+Console.WriteLine("  ");
